041695fdd7192b5029a80696e6b348aa6db82e14
Lautaro Orellano <Lautaa123@gmail.com>
2025-10-13 13:06:04 -0300

    nuevo metodos para implementar en mainFx

--- src/interfaces/IProducManager.java
+++ src/interfaces/IProducManager.java
@@ -1,10 +1,16 @@
 package interfaces;
 
+import clases.entidades.Product;
+
+import java.util.List;
+
 public interface IProducManager {
 
     void createProduct(String name, String description, double price,int stock);
     void getAllProducts();
+    List<Product> getProducts();
     void searchProduct(int id);
+    Product searchProductFX(int id);
     void updateProduct(int id, String name, String description, double price, int stock);
     void deleteProduct(int id);
 }
