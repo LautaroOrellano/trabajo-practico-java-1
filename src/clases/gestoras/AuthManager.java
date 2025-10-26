4eeabc1dfb9256e16349e9dde5024fa520c6ec3b
Lautaro Orellano <Lautaa123@gmail.com>
2025-10-26 15:37:46 -0300

    chore

--- src/clases/gestoras/AuthManager.java
+++ src/clases/gestoras/AuthManager.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AuthManager {
-    private List<User> users = new ArrayList<>();
+    private List<User> users;
 
     public AuthManager(List<User> users) {
         this.users = users;
