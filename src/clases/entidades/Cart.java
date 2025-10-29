eea33ed83a6cf0b526bd99933e72ce142e42367f
Lautaro Orellano <lautaa123@gmail.com>
2025-10-29 16:06:22 -0300

    refactor: futura implementacion de json

--- src/clases/entidades/Cart.java
+++ src/clases/entidades/Cart.java
@@ -1,5 +1,9 @@
 package clases.entidades;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,6 +35,28 @@ public class Cart {
                 .mapToDouble(Product::getPrice)
                 .sum();
     }
+
+    /*public static Cart fromJson(JSONObject json) {
+        Cart cart = new Cart();
+        JSONArray array = json.getJSONArray("products");
+        try {
+            for (int i = 0; i < array.length(); i++) {
+                JSONObject productJson = array.getJSONObject(i);
+                Product p = new Product(
+                        productJson.getString("name"),
+                        productJson.getString("description"),
+                        productJson.getDouble("price"),
+                        productJson.getInt("stock")
+                );
+                cart.addProduct(p);
+            }
+        } catch (JSONException e) {
+            System.out.println("Problema al convertir el json");
+        }
+
+
+        return cart;
+    }*/
 
     public boolean isEmpty() {
         return products.isEmpty();
